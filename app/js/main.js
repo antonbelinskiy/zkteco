94fb5dadb4f83f85d395abaa6c72ca8a1605170c
antonbelinskiy <tohaantonishe@gmail.com>
2021-01-22 21:21:22 +0200

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,11 @@
 $(function () {
-    // $('.slider').slick({
-    //     infinite: true,
-    //     fade: true
-    // })
+    $('.slider_inner').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider_arrows slider_arrows-left" src="../img/arrow.svg" alt="Arrow">',
+        nextArrow: '<img class="slider_arrows slider_arrows-right" src="../img/arrow.svg" alt="Arrow">',
+        dots: true
+    })
 
 
 
